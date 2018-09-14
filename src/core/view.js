372cc776b58fa0fecb0d9d99ee02b530555a04ee
Raybeedrill <demezis1994@gmal.com>
2018-09-14 05:02:38 +0300

    added view core

---
+++ src/core/view.js
@@ -0,0 +1,59 @@
+class View {
+    constructor(format, data, codeObj, resp) {
+        this.format = format;
+        this.data = data;
+        this.codeObj = codeObj;
+        this.response = resp;
+        this.formatData().sendResponse(this.codeObj, this.data);
+    }
+
+    formatData() {
+        switch (this.format) {
+            case '.json':
+                this.data = this.toJSON();
+                break;
+            case '.xml':
+                this.data = this.toXML();
+                break;
+            case '.html':
+                this.data = this.toHTML();
+                break;
+            case '.txt':
+                this.data = this.toText();
+                break;
+        }
+        return this;
+    }
+    toJSON() {
+        return JSON.stringify(this.data);
+    }
+    //todo
+    toXML() {
+        return this.data.toString();
+    }
+    toHTML() {
+        return [
+            '<html>',
+            '<head>',
+            '</head>',
+            '<body>',
+            this.data.toString(),
+            '</body>',
+            '</html>'
+        ].toString();
+    }
+    toText() {
+        return this.data.toString();
+    }
+
+    sendResponse(codeObj, sendData) {
+        this.response.status(codeObj.code);
+        if (codeObj.code == '200') {
+            this.response.send(sendData);
+            return;
+        }
+        this.response.send(codeObj.description);
+    }
+}
+
+module.exports = View;
